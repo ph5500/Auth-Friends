595551d9fd406b3eefa9ec8c2a2425512c784609
Pinefoot <jameslcarpino@gmail.com>
2020-04-21 13:10:29 -0700

    initialized and set up project.

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
